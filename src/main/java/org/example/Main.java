b6645e0e7463e51a120cc19fc97f0ab77ed0b6b3
prashant kumar <prashantkum7676@gmail.com>
2025-12-28 10:59:18 +0530

    arraylist code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,22 @@
 package org.example;
 
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Array
+      ArrayList<Integer> List = new ArrayList<>();
+List.add(1);
+List.add(5);
+List.add(80);
+        System.out.println(List.get(2));
+        System.out.println(List.size());
+        for (int i = 0;i< List.size();i++){
+            System.out.println(List.get(i));
+        }
     }
 }
 
